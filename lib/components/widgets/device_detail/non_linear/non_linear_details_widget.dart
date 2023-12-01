b71c4f691d8e2e67a9a8708dc4f4a11a41966dd7
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2023-12-01 21:06:59 +0530

    feat: added non linear reading

---
+++ lib/components/widgets/device_detail/non_linear/non_linear_details_widget.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:ultra_level_pro/ble/ultra_level_helpers/constant.dart';
+import 'package:ultra_level_pro/components/widgets/common/common.dart';
+import 'package:ultra_level_pro/components/widgets/common/input.dart';
+
+const headerStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w900,
+);
+
+const bodyStyle = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w900,
+);
+
+Widget headerText(String text) {
+  return Text(
+    text,
+    style: headerStyle,
+  );
+}
+
+Widget bodyText(String text) {
+  return Text(
+    text,
+    style: bodyStyle,
+  );
+}
+
+class NonLinearTankDetailsWidget extends StatelessWidget {
+  const NonLinearTankDetailsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [1, 2, 3, 45, 5].map((e) {
+        return SingleNonLinearTankDetailsWidget(
+          index: e,
+          height: 10,
+          filled: 10,
+          onRemove: (index) {},
+          onHeightChange: (index, height) async {},
+          onFilledChange: (index, filled) async {},
+        );
+      }).toList(),
+    );
+  }
+}
+
+class SingleNonLinearTankDetailsWidget extends StatelessWidget {
+  const SingleNonLinearTankDetailsWidget({
+    super.key,
+    required this.index,
+    required this.height,
+    required this.filled,
+    required this.onRemove,
+    required this.onHeightChange,
+    required this.onFilledChange,
+  });
+  final int index;
+  final double height;
+  final double filled;
+  final void Function(int index) onRemove;
+  final Future<void> Function(int index, String height) onHeightChange;
+  final Future<void> Function(int index, String filled) onFilledChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constrain) {
+      return SizedBox(
+        width: constrain.maxWidth,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Tank ${index + 1}',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      onRemove(index);
+                    },
+                    child: const Text(
+                      'Remove',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 25,
+              ),
+              Flexible(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Height:',
+                          style: bodyStyle,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          height.toString(),
+                          style: bodyStyle,
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: Input(
+                            hintText: "Tank Height",
+                            onDone: (_, val) {
+                              return onHeightChange(index, val);
+                            },
+                            parameter: WriteParameter.TankHeight,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Filled:',
+                          style: bodyStyle,
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Text(
+                          height.toString(),
+                          style: bodyStyle,
+                        ),
+                        const SizedBox(
+                          width: 24,
+                        ),
+                        Expanded(
+                          child: Input(
+                            hintText: "Tank Filled",
+                            onDone: (_, val) {
+                              return onHeightChange(index, val);
+                            },
+                            parameter: WriteParameter.TankHeight,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+}
--- lib/components/widgets/device_detail/tank_details_widget.dart
+++ lib/components/widgets/device_detail/tank_details_widget.dart
@@ -6,8 +6,9 @@ import 'package:ultra_level_pro/ble/ultra_level_helpers/tank_type.dart';
 import 'package:ultra_level_pro/ble/ultra_level_helpers/tank_type_changer.dart';
 import 'package:ultra_level_pro/components/widgets/common/common.dart';
 import 'package:ultra_level_pro/components/widgets/common/input.dart';
+import 'package:ultra_level_pro/components/widgets/device_detail/non_linear/non_linear_create_widget.dart';
+import 'package:ultra_level_pro/components/widgets/device_detail/non_linear/non_linear_details_widget.dart';
 import 'package:ultra_level_pro/components/widgets/device_detail/shapes_widget.dart';
-import 'package:ultra_level_pro/components/widgets/device_detail/tank_type_changer/non_linear_widget.dart';
 
 const headerStyle = TextStyle(
   fontSize: 16,
@@ -193,169 +194,185 @@ class _TankDetailsWidgetState extends State<TankDetailsWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        shape(widget.state?.tankType),
-        Table(
-          defaultColumnWidth: FlexColumnWidth(2),
+    return SingleChildScrollView(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          // Set a minimum height equal to the screen height
+          minHeight: MediaQuery.of(context).size.height,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
           children: [
-            TableRow(
+            shape(widget.state?.tankType),
+            Table(
+              defaultColumnWidth: FlexColumnWidth(2),
               children: [
-                headerText("Tank type"),
-                bodyText(': ${widget.state?.tankType.name}'),
-                formItem(
-                  DropdownButton<String>(
-                    isExpanded: true,
-                    focusColor: Colors.green,
-                    style: const TextStyle(
-                      color: Colors.black87,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                TableRow(
+                  children: [
+                    headerText("Tank type"),
+                    bodyText(': ${widget.state?.tankType.name}'),
+                    formItem(
+                      DropdownButton<String>(
+                        isExpanded: true,
+                        focusColor: Colors.green,
+                        style: const TextStyle(
+                          color: Colors.black87,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        underline: Container(),
+                        value: widget.state?.tankType.name ?? tankTypes[0],
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        items: tankTypes.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Text(items),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          if (newValue != null) {
+                            final tankType = TankType.values.firstWhere(
+                                (element) => element.name == newValue);
+                            if (tankType != TankType.nonLinear) {
+                              widget.onDone(WriteParameter.TankType,
+                                  getTankTypeInHex(tankType));
+                            } else {
+                              bottomSheetBuilder(context, tankType);
+                            }
+                          }
+                        },
+                      ),
+                    )
+                  ],
+                ),
+                tableGap(),
+                if (widget.state?.tankType != TankType.nonLinear) ...[
+                  TableRow(
+                    children: [
+                      headerText("Tank offset mm"),
+                      bodyText(': ${widget.state?.tankOffset}'),
+                      formItem(
+                        Input(
+                          hintText: "Tank offset",
+                          onDone: widget.onDone,
+                          parameter: WriteParameter.TankOffset,
+                        ),
+                      )
+                    ],
+                  ),
+                  tableGap(),
+                  TableRow(
+                    children: [
+                      headerText("Tank height mm"),
+                      bodyText(': ${widget.state?.tankHeight}'),
+                      formItem(
+                        Input(
+                          hintText: "Tank height",
+                          onDone: widget.onDone,
+                          parameter: WriteParameter.TankHeight,
+                        ),
+                      )
+                    ],
+                  ),
+                  tableGap(),
+                  TableRow(
+                    children: [
+                      headerText("Tank Length mm"),
+                      bodyText(': ${widget.state?.tankLength}'),
+                      formItem(
+                        Input(
+                          hintText: "Tank Length",
+                          onDone: widget.onDone,
+                          parameter: WriteParameter.TankLength,
+                        ),
+                      )
+                    ],
+                  ),
+                  if (widget.state?.tankType == TankType.rectangle ||
+                      widget.state?.tankType == TankType.horizontalOval) ...[
+                    tableGap(),
+                    TableRow(
+                      children: [
+                        headerText("Tank Width mm"),
+                        bodyText(': ${widget.state?.tankWidth}'),
+                        formItem(
+                          Input(
+                            hintText: "Tank Length",
+                            onDone: widget.onDone,
+                            parameter: WriteParameter.TankLength,
+                          ),
+                        )
+                      ],
                     ),
-                    underline: Container(),
-                    value: widget.state?.tankType.name ?? tankTypes[0],
-                    icon: const Icon(Icons.keyboard_arrow_down),
-                    items: tankTypes.map((String items) {
-                      return DropdownMenuItem(
-                        value: items,
-                        child: Text(items),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      if (newValue != null) {
-                        final tankType = TankType.values
-                            .firstWhere((element) => element.name == newValue);
-                        if (tankType != TankType.nonLinear) {
-                          widget.onDone(WriteParameter.TankType,
-                              getTankTypeInHex(tankType));
-                        } else {
-                          bottomSheetBuilder(context, tankType);
-                        }
-                      }
-                    },
-                  ),
-                )
-              ],
-            ),
-            tableGap(),
-            TableRow(
-              children: [
-                headerText("Tank offset mm"),
-                bodyText(': ${widget.state?.tankOffset}'),
-                formItem(
-                  Input(
-                    hintText: "Tank offset",
-                    onDone: widget.onDone,
-                    parameter: WriteParameter.TankOffset,
-                  ),
-                )
-              ],
-            ),
-            tableGap(),
-            TableRow(
-              children: [
-                headerText("Tank height mm"),
-                bodyText(': ${widget.state?.tankHeight}'),
-                formItem(
-                  Input(
-                    hintText: "Tank height",
-                    onDone: widget.onDone,
-                    parameter: WriteParameter.TankHeight,
-                  ),
-                )
-              ],
-            ),
-            tableGap(),
-            TableRow(
-              children: [
-                headerText("Tank Length mm"),
-                bodyText(': ${widget.state?.tankLength}'),
-                formItem(
-                  Input(
-                    hintText: "Tank Length",
-                    onDone: widget.onDone,
-                    parameter: WriteParameter.TankLength,
-                  ),
-                )
-              ],
-            ),
-            if (widget.state?.tankType == TankType.rectangle ||
-                widget.state?.tankType == TankType.horizontalOval) ...[
-              tableGap(),
-              TableRow(
-                children: [
-                  headerText("Tank Width mm"),
-                  bodyText(': ${widget.state?.tankWidth}'),
-                  formItem(
-                    Input(
-                      hintText: "Tank Length",
-                      onDone: widget.onDone,
-                      parameter: WriteParameter.TankLength,
+                  ] else ...[
+                    tableGap(),
+                    TableRow(
+                      children: [
+                        headerText("Tank Diameter mm"),
+                        bodyText(': ${widget.state?.tankDiameter}'),
+                        formItem(
+                          Input(
+                            hintText: "Tank Diameter",
+                            onDone: widget.onDone,
+                            parameter: WriteParameter.TankLength,
+                          ),
+                        )
+                      ],
                     ),
-                  )
+                  ],
+                  tableGap(),
+                  TableRow(
+                    children: [
+                      headerText("Low level Relay mm"),
+                      bodyText(": ${widget.state?.lowLevelRelayInMm} "),
+                      formItem(
+                        Input(
+                          hintText: "Low level relay",
+                          onDone: widget.onDone,
+                          parameter: WriteParameter.LowLevelRelayInMm,
+                        ),
+                      )
+                    ],
+                  ),
+                  tableGap(),
+                  TableRow(
+                    children: [
+                      headerText("High level Relay mm"),
+                      bodyText(": ${widget.state?.highLevelRelayInPercent}"),
+                      formItem(
+                        Input(
+                          hintText: "High level relay",
+                          onDone: widget.onDone,
+                          parameter: WriteParameter.HighLevelRelayInPercent,
+                        ),
+                      )
+                    ],
+                  ),
+                  tableGap(),
+                  TableRow(
+                    children: [
+                      headerText("LPH"),
+                      bodyText(": ${widget.state?.lph}"),
+                      formItem(
+                        Input(
+                          hintText: "Liters per hour",
+                          onDone: widget.onDone,
+                          parameter: WriteParameter.Lph,
+                        ),
+                      )
+                    ],
+                  ),
                 ],
-              ),
-            ] else ...[
-              tableGap(),
-              TableRow(
-                children: [
-                  headerText("Tank Diameter mm"),
-                  bodyText(': ${widget.state?.tankDiameter}'),
-                  formItem(
-                    Input(
-                      hintText: "Tank Diameter",
-                      onDone: widget.onDone,
-                      parameter: WriteParameter.TankLength,
-                    ),
-                  )
-                ],
-              ),
-            ],
-            tableGap(),
-            TableRow(
-              children: [
-                headerText("Low level Relay mm"),
-                bodyText(": ${widget.state?.lowLevelRelayInMm} "),
-                formItem(
-                  Input(
-                    hintText: "Low level relay",
-                    onDone: widget.onDone,
-                    parameter: WriteParameter.LowLevelRelayInMm,
-                  ),
-                )
               ],
             ),
-            tableGap(),
-            TableRow(
-              children: [
-                headerText("High level Relay mm"),
-                bodyText(": ${widget.state?.highLevelRelayInPercent}"),
-                formItem(
-                  Input(
-                    hintText: "High level relay",
-                    onDone: widget.onDone,
-                    parameter: WriteParameter.HighLevelRelayInPercent,
-                  ),
-                )
-              ],
-            ),
-            tableGap(),
-            TableRow(
-              children: [
-                headerText("LPH"),
-                bodyText(": ${widget.state?.lph}"),
-                formItem(
-                  Input(
-                    hintText: "Liters per hour",
-                    onDone: widget.onDone,
-                    parameter: WriteParameter.Lph,
-                  ),
-                )
-              ],
-            ),
+            if (widget.state?.tankType == TankType.nonLinear) ...[
+              const NonLinearTankDetailsWidget()
+            ]
           ],
         ),
-      ],
+      ),
     );
   }
 }
